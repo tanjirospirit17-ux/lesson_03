9c6e3f15ced2ca16a01a3672691194d66289d130
tanjirospirit17-ux <tanjirospirit17@gmail.com>
2026-07-03 14:13:57 +0300

    Clean main branch

--- address.py
+++
@@ -1,7 +0,0 @@
-class Address:
-    def __init__(self, zip_code, city, street, house, apartment):
-        self.zip_code = zip_code
-        self.city = city
-        self.street = street
-        self.house = house
-        self.apartment = apartment
--- lesson_3_task_1.py
+++
@@ -1,7 +0,0 @@
-from user import User
-
-my_user = User("Huy", "Nguyen")
-
-my_user.print_first_name()
-my_user.print_last_name()
-my_user.print_full_name()
